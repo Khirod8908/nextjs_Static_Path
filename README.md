9504fbb0a37d20ba3399321d86844a72f6e19184
Khirod8908 <khirodkumarpanda661@gmail.com>
2022-08-27 16:24:49 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-5pd9td
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-5pd9td)
---
+++ pages/[pId].js
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+export default function Home({ data }) {
+  return (
+    <div>
+      <Link href="/">Back</Link>
+      <hr />
+      <p>{data.name}</p>
+    </div>
+  );
+}
+
+const fetchURL = 'https://jsonplaceholder.typicode.com/users';
+
+export async function getStaticPaths() {
+  const res = await fetch(fetchURL);
+  const data = await res.json();
+  const paths = data.map((item) => ({
+    params: { pId: item.id.toString() },
+  }));
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const productId = context.params.pId;
+  const res = await fetch(`${fetchURL}/${productId}`);
+  const data = await res.json();
+  return { props: { data } };
+}
