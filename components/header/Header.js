0fa100ee0c1d78d503855f2ed7048b6325017b3c
Khirod8908 <khirodkumarpanda661@gmail.com>
2022-08-30 11:14:55 +0530

    2 files modified, 2 added

--- components/header/Header.js
+++ components/header/Header.js
@@ -1,7 +1,20 @@
+import classes from './header.module.css';
+import Link from 'next/link';
 export default () => {
   return (
-    <header>
-      
+    <header className={`container-fluid ${classes.header}`}>
+      <nav className="container d-flex align-items-center justify-content-between">
+        <div className="logo">
+          <img src="/images/Logo.svg" alt="EDUCARE" />
+        </div>
+        <ul className={`d-flex align-items-center mb-0 ${classes.nav_links}`}>
+          <Link href="/">Home</Link>
+          <li>Categories</li>
+          <Link href="/">About Us</Link>
+          <Link href="/">Contact Us</Link>
+        </ul>
+        <div className="icon">icons</div>
+      </nav>
     </header>
   );
 };
